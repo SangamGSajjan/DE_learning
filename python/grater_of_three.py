e4f7992902aa3d436a7f650f652d4071446b4354
Sangam G Sajjan <sangamgsajjan0987@gmail.com>
2024-12-02 23:34:26 +0530

    optimize greater in three variales

--- python/grater_of_three.py
+++ python/grater_of_three.py
@@ -2,12 +2,21 @@ x = int(input("enter x"))
 y = int(input("enter y"))
 z = int(input("enter z"))
 
-if(x>y):
-    if(x>z):
-        print("x is grater")
-    else:
-        print("z is grater")
-elif(y>z):
+# if(x>y):
+#     if(x>z):
+#         print("x is grater")
+#     else:
+#         print("z is grater")
+# elif(y>z):
+#      print("y is grater")
+# else:
+#     print("z is grater")
+
+
+
+if(x>y & x>z):
+    print("x is grater")
+elif(y>z & y>x):
      print("y is grater")
 else:
     print("z is grater")
